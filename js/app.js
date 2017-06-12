87d4ed5f7771c0de26273f1f9b87793647d33834
Tyler W <tyler.walser@yahoo.com>
2017-06-12 16:31:17 -0700

    finished js for first store

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 'use strict';
 
 var store1 = {
-  name: firstAndPike,
+  name: 'firstAndPike',
   cookieAverage: 6.3,
-  customers: function(){
-    this.customers = randomCustomers(23,65);
+  hoursOpen: ['8am', '9am', '10am','11am', '12pm', '1pm','2pm', '3pm', '4pm','5pm', '6pm', '7pm','8pm'],
+  getCustomers: function(){
+    return randomCustomers(23,65);
   },
-  cookiesSold: function (){
-    Math.round(customers * cookieAverage) + ' cookies';
+  cookiesSold: function(){
+    return Math.round(this.getCustomers() * this.cookieAverage);
   },
-  hoursOpen: ['8am', '9am', '10am','11am', '12pm', '1pm','2pm', '3pm', '4pm','5pm', '6pm', '7pm','8pm']
-
 };
+
+console.log(store1.getCustomers());
+console.log(store1.cookiesSold());
 
 function randomCustomers (min,max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 
 var parentElement = document.getElementById('sales');
 
@@ -34,13 +35,18 @@ article.appendChild(p);
 var ul = document.createElement('ul');
 article.appendChild(ul);
 
+var totalCookies = 0;
+
 for (var i = 0; i < store1.hoursOpen.length; i++) {
   var li = document.createElement ('li');
-  li.textContent = store1.hoursOpen [i] + ': ' + store1.cookiesSold;
+  li.textContent = store1.hoursOpen [i] + ': ' + store1.cookiesSold();
+  (totalCookies += store1.cookiesSold());
   ul.appendChild(li);
 }
+
+li.textContent = 'Total: ' + totalCookies;
 
 var img = document.createElement('img');
 img.setAttribute('src', 'images/salmon.png');
 img.setAttribute('alt', 'cute picture of a salmon');
-article.appentChild(img);
+article.appendChild(img);
